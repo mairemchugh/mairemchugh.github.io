275c2f62727c4f3f02bf4557d3cbc74e42cc5777
mairemchugh <mamc5457@colorado.edu>
2022-04-03 21:12:05 -0600

    blah dee dah

--- java/index1.js
+++ java/index1.js
@@ -1,11 +1,16 @@
 function button1Clicked() {
-  document.getElementById('button-1').classic('was-clicked');
+  document.getElementById('button-1').classList.toggle('was-clicked');
 
+}
+
+function button2Clicked() {
+  document.getElementById('button-2').classList.toggle('was-clicked');
 }
 
 function windowLoaded() {
   console.log('loaded ok!'):
   document.getElementById('button-1').addEventListener('click', button1Clicked);
+  document.getElementById('button-2').addEventListener('click', button2Clicked);
 
 }
 
