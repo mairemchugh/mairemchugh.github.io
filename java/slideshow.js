9279e0bb2568d1ed7a9d8acc2a64151eabe3963d
mairemchugh <mamc5457@colorado.edu>
2022-05-03 14:16:20 -0600

    i7giffil

--- java/slideshow.js
+++ java/slideshow.js
@@ -1,3 +1,47 @@
+const slideshowContainer = document.querySelector(".slideshow-container");
+const myImages = document.querySelector(".mySlides img");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+
+let counter = 1;
+const size = myImages[0].clientWidth;
+
+
+slideshowContainer.style.transform = "translateX(" + ( - size * counter)+ px")";
+
+
+nextBtn.addEventListener("click",()=>) {
+  slideshowContainer.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  slideshowContainer.style.transform = "translateX(" + ( - size * counter)+ px)";
+});
+
+
+prevBtn.addEventListener("click",()=>) {
+  slideshowContainer.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  slideshowContainer.style.transform = "translateX(" + ( - size * counter)+ px)";
+});
+
+
+slideshowContainer.addEventListener("transitionend", ()=> {
+  if (myImages[counter].id === "lastClone") {
+    slideshowContainer.style.transition = "none";
+    counter = myImages.length -2;
+    slideshowContainer.style.transform = "translateX(" + ( - size * counter)+ px)";
+  }
+  if (myImages[counter].id === "firstClone") {
+    slideshowContainer.style.transition = "none";
+    counter = myImages.length - counter;
+    slideshowContainer.style.transform = "translateX(" + ( - size * counter)+ px)";
+  }
+
+});
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
